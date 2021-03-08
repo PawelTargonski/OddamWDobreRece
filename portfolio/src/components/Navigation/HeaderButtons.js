7f031d971120d04dfd814d2091a956de71e0e917
PawelTargonski <pawel.targonski@gmail.com>
2021-03-08 02:35:36 +0100

    header navi

---
+++ portfolio/src/components/Navigation/HeaderButtons.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Buttons = () => (
+    <>
+    <button className='HeaderButton1'>oddaj <br/>rzeczy</button>
+    <button className='HeaderButton2'>zorganizuj <br/>zbiórke</button>
+    </>
+)
+
+export default Buttons;
--- portfolio/src/components/Navigation/Navigation.js
+++ portfolio/src/components/Navigation/Navigation.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
+import Buttons from './HeaderButtons.js';
 const Navigation = () => (
     <div className="Header">
-            
-            <div className="Menu">
-                <ul class="UpperMenuHeaderNavigator">
-                    <li  class="MenuHeaderListUp"><Link class="MenuHeaderListUpLink" to={ROUTES.LOGIN}>Zaloguj</Link></li>
-                    <li  class="MenuHeaderListUp"><Link class="MenuHeaderListUpLink" to={ROUTES.REGISTER}>Załóż konto</Link></li>
-                </ul>
-                <ul className="MenuHeaderNavigatior">
-                    <li  class="MenuHeaderList"><Link class="MenuHeaderListLink" to={ROUTES.HOME}>Start</Link></li>    
-                    <li  class="MenuHeaderList"><Link class="MenuHeaderListLink" to={ROUTES.ABOUTUS}>O nas</Link></li> 
-                    <li  class="MenuHeaderList"><Link class="MenuHeaderListLink" to={ROUTES.WHODOWEHELP}>Fundacja i organizacje</Link></li> 
-                    <li  class="MenuHeaderList"><Link class="MenuHeaderListLink" to={ROUTES.CONTACT}>Kontakt</Link></li> 
-                </ul>
-            </div>
             <div className='HeroImage'/>
-            <div className='HeaderHero'>
-                <p>Zacznij pomagać!<br/>
-                    Oddaj niechciane rzeczy w dobre ręce!
-                </p>
+            <div className='HeaderContainer'>
+                <div className="Menu">
+                    <ul class="UpperMenuHeaderNavigator">
+                        <li  class="MenuHeaderListUp"><Link class="MenuHeaderListUpLink" to={ROUTES.LOGIN}>Zaloguj</Link></li>
+                        <li  class="MenuHeaderListUp"><Link class="MenuHeaderListUpLink" to={ROUTES.REGISTER}>Załóż konto</Link></li>
+                    </ul>
+                    <ul className="MenuHeaderNavigatior">
+                        <li  class="MenuHeaderList"><Link class="MenuHeaderListLink" to={ROUTES.HOME}>Start</Link></li>    
+                        <li  class="MenuHeaderList"><Link class="MenuHeaderListLink" to={ROUTES.ABOUTUS}>O nas</Link></li> 
+                        <li  class="MenuHeaderList"><Link class="MenuHeaderListLink" to={ROUTES.WHODOWEHELP}>Fundacja i organizacje</Link></li> 
+                        <li  class="MenuHeaderList"><Link class="MenuHeaderListLink" to={ROUTES.CONTACT}>Kontakt</Link></li> 
+                    </ul>
+                </div>
+                <div className='HeaderHero'>
+                    <p>Zacznij pomagać!<br/>
+                        Oddaj niechciane rzeczy w zaufane ręce!
+                    </p>
+                    <div className='HeroDecoration'/>
+                    <Buttons/>
+                </div>
             </div>
     </div>
 )
